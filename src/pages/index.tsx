0e7a49edc5e78e70088d1650edc7db3f086e4a3c
betolarbac <betocr.dev@gmail.com>
2022-03-09 22:15:45 -0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns'
+import ptBr from 'date-fns/locale/pt-BR'
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/ConvertDurationToTimeString';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
 
 
 
-export default function Home(props) {
+export default function Home(props: HomeProps ) {
   return (
    <div>
        <h1>Index</h1>
@@ -10,9 +25,29 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps:GetStaticProps = async () => {
+  const { data } = await api.get('episodes?', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  })
+
+  const episodes = data.map(episode =>{
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBr}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.url,
+    }
+  })
+ 
 
   return {
     props: {
