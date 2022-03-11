913330257af1681dd1f049b9eb3c3a11cc6b9b18
betolarbac <betocr.dev@gmail.com>
2022-03-10 21:28:16 -0300

    api e home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,68 @@ import { format, parseISO } from 'date-fns'
 import ptBr from 'date-fns/locale/pt-BR'
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/ConvertDurationToTimeString';
+import Image from 'next/Image'
+
+import styles from './home.module.scss'
 
 type Episode = {
   id: string;
   title: string;
   members: string;
+  thumbnail: string;
+  description: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  published_at: string;
 }
 
 type HomeProps = {
-  episodes: Episode[];
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
 
 
-export default function Home(props: HomeProps ) {
+export default function Home({latestEpisodes, allEpisodes}: HomeProps ) {
   return (
-   <div>
-       <h1>Index</h1>
-       <p>{JSON.stringify(props.episodes)}</p>
+   <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Mídias recentes</h2>
+
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id} >
+                  <Image
+                    width={192}
+                    height={192}
+                   src={episode.thumbnail} 
+                   alt={episode.title} 
+                   objectFit="cover"
+                   />
+
+
+                  <div className={styles.episodeDetails}>
+                      <a href="">{episode.title}</a>
+                      <p>{episode.members}</p>
+                      <span>{episode.published_at}</span>
+                      <span>{episode.durationAsString}</span>
+                  </div>
+
+                  <button type="button">
+                    <img src="/play-green.svg" alt="Tocar episodio" />
+                  </button>
+              </li>
+            )
+          })}
+
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+
+      </section>
    </div>
   )
 }
@@ -44,14 +88,18 @@ export const getStaticProps:GetStaticProps = async () => {
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
       description: episode.description,
-      url: episode.url,
+      url: episode.file.url,
     }
   })
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
  
 
   return {
     props: {
-      episodes: data,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8,
   }
--- src/utils/ConvertDurationToTimeString.ts
+++ src/utils/ConvertDurationToTimeString.ts
@@ -3,10 +3,11 @@ export function convertDurationToTimeString(duration: number) {
     const minutes = Math.floor((duration % 3600)/ 60);
     const seconds = duration % 60;
 
-    const timeString =[hours, minutes, seconds]
+    const timeString = [hours, minutes, seconds]
     .map(unit => String(unit).padStart(2, '0'))
     .join(':')
 
 
     return timeString;
 }
+
